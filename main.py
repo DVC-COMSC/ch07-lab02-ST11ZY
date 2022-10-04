5d38064e1aa67b44e10e052509f6e962f38a7fc6
Your Name <you@example.com>
2022-10-04 13:03:53 -0700

    code

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # ******************************
 # Make your Code
 # ******************************
+import random
+list=[]
+nums=0
+while nums<10:
+    list.append(random.randint(0,100))
+    nums=nums+1
+smallestNum=min(list)
+smallestIn=list.index(smallestNum)
+print("Min Val:"+str(smallestNum)+" Index:"+str(smallestIn))
